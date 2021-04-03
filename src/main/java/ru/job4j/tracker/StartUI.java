e71404e2a640002f3036e4510f0381b6cd6256b2
dmitriiibulgakov <dmitriii.bulgakov@gmail.com>
2021-04-03 12:17:15 +0700

    4.2. Статические методы. [#491846]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,6 +1,66 @@
 package ru.job4j.tracker;
 
 public class StartUI {
+    public static void createItem(Input input, Tracker tracker) {
+        System.out.println("=== Create a new Item ====");
+        String name = input.askStr("Enter name: ");
+        Item item = new Item(name);
+        tracker.add(item);
+    }
+
+    public static void showAllItems(Input input, Tracker tracker) {
+        System.out.println("=== Show all items ====");
+        Item[] items = tracker.findAll();
+        for (Item item : items) {
+            System.out.println(item);
+        }
+    }
+
+    public static void editItem(Input input, Tracker tracker) {
+        System.out.println("=== Edit item ====");
+        String id = input.askStr("Enter item's id: ");
+        String name = input.askStr("Enter new name: ");
+        Item item = new Item(name);
+        if (tracker.replace(Integer.parseInt(id), item)) {
+            System.out.println("Completed successfully");
+        } else {
+            System.out.println("Something went wrong");
+        }
+    }
+
+    public static void deleteItem(Input input, Tracker tracker) {
+        System.out.println("=== Delete item ====");
+        String id = input.askStr("Enter item's id: ");
+        if (tracker.delete(Integer.parseInt(id))) {
+            System.out.println("Completed successfully");
+        } else {
+            System.out.println("Something went wrong");
+        }
+    }
+
+    public static void findItemById(Input input, Tracker tracker) {
+        System.out.println("=== Find item by Id ====");
+        String id = input.askStr("Enter item's id: ");
+        Item item = tracker.findById(Integer.parseInt(id));
+        if (item == null) {
+            System.out.println("Заявка с таким id не найдена");
+        } else {
+            System.out.println(item);
+        }
+    }
+
+    public static void findItemByName(Input input, Tracker tracker) {
+        System.out.println("=== Find items by name ====");
+        String name = input.askStr("Enter item's id: ");
+        Item[] items = tracker.findByName(name);
+        if (items.length > 0) {
+            for (Item item : items) {
+                System.out.println(item);
+            }
+        } else {
+            System.out.println("Items with the entered name not found");
+        }
+    }
 
     public void init(Input input, Tracker tracker) {
         boolean run = true;
@@ -8,54 +68,17 @@ public class StartUI {
             this.showMenu();
             int select = Integer.parseInt(input.askStr("Select: "));
             if (select == 0) {
-                System.out.println("=== Create a new Item ====");
-                String name = input.askStr("Enter name: ");
-                Item item = new Item(name);
-                tracker.add(item);
+                createItem(input, tracker);
             } else if (select == 1) {
-                System.out.println("=== Show all items ====");
-                Item[] items = tracker.findAll();
-                for (Item item : items) {
-                    System.out.println(item);
-                }
+                showAllItems(input, tracker);
             } else if (select == 2) {
-                System.out.println("=== Edit item ====");
-                String id = input.askStr("Enter item's id: ");
-                String name = input.askStr("Enter new name: ");
-                Item item = new Item(name);
-                if (tracker.replace(Integer.parseInt(id), item)) {
-                    System.out.println("Completed successfully");
-                } else {
-                    System.out.println("Something went wrong");
-                }
+                editItem(input, tracker);
             } else if (select == 3) {
-                System.out.println("=== Delete item ====");
-                String id = input.askStr("Enter item's id: ");
-                if (tracker.delete(Integer.parseInt(id))) {
-                    System.out.println("Completed successfully");
-                } else {
-                    System.out.println("Something went wrong");
-                }
+                deleteItem(input, tracker);
             } else if (select == 4) {
-                System.out.println("=== Find item by Id ====");
-                String id = input.askStr("Enter item's id: ");
-                Item item = tracker.findById(Integer.parseInt(id));
-                if (item == null) {
-                    System.out.println("Заявка с таким id не найдена");
-                } else {
-                    System.out.println(item);
-                }
+                findItemById(input, tracker);
             } else if (select == 5) {
-                System.out.println("=== Find items by name ====");
-                String name = input.askStr("Enter item's id: ");
-                Item[] items = tracker.findByName(name);
-                if (items.length > 0) {
-                    for (Item item : items) {
-                        System.out.println(item);
-                    }
-                } else {
-                    System.out.println("Items with the entered name not found");
-                }
+                findItemByName(input, tracker);
             } else if (select == 6) {
                 run = false;
             }
