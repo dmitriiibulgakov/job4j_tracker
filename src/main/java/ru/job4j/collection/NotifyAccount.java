0cc2f246105f5fb144acd31a1b9063556be0f585
dmitriiibulgakov <dmitriii.bulgakov@gmail.com>
2021-04-11 20:47:21 +0700

    3. Модели данных и HashSet. [#491833]

---
+++ src/main/java/ru/job4j/collection/NotifyAccount.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class NotifyAccount {
+    public static Set<Account> sent(List<Account> accounts) {
+        Set<Account> rsl = new HashSet<>();
+        for (Account account: accounts) {
+            rsl.add(account);
+        }
+        return rsl;
+    }
+}
