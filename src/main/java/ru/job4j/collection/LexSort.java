9f93ec5cdca690737abdf1c5256678064f833a20
dmitriiibulgakov <dmitriii.bulgakov@gmail.com>
2021-04-21 20:27:26 +0700

    4. Сортировка номера [#491813]

---
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,12 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+    private static final String DOT = "\\.";
+
+    @Override
+    public int compare(String left, String right) {
+        return Integer.compare(Integer.parseInt(left.split(DOT)[0]), Integer.parseInt(right.split(DOT)[0]));
+    }
+}
