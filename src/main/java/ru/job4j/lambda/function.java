df0e6785355ae99a92e5372c65d78ea9b11fffae
dmitriiibulgakov <dmitriii.bulgakov@gmail.com>
2021-05-20 22:06:23 +0700

    5. Подсчет функции в диапазоне. [#24251]

---
+++ src/main/java/ru/job4j/lambda/function.java
@@ -0,0 +1,15 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class function {
+    public static List<Double> diapason(int start, int end, Function<Double, Double> func) {
+        List<Double> res = new ArrayList<>();
+        for (double i = start; i < end; i++) {
+            res.add(func.apply(i));
+        }
+        return res;
+    }
+}
---
+++ src/test/java/ru/job4j/lambda/functionTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.lambda;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class functionTest {
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        List<Double> result = function.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenQuadraticFunctionThenQuadraticResults() {
+        List<Double> result = function.diapason(1, 4, x -> Math.pow(x, 2));
+        List<Double> expected = Arrays.asList(1D, 4D, 9D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenExponentialFunctionThenExponentialResults() {
+        List<Double> result = function.diapason(2, 5, x -> Math.pow(2, x));
+        List<Double> expected = Arrays.asList(4D, 8D, 16D);
+        assertThat(result, is(expected));
+    }
+}
