e4673e7817ac31677f81ebcb67308ea4ccfa742d
dmitriiibulgakov <dmitriii.bulgakov@gmail.com>
2021-05-24 21:48:39 +0700

    3. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,12 +1,16 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles){
+        Comparator<Address> comparator = (Comparator.comparing(Address::getCity));
         return profiles.stream()
                 .map(Profile::getAddress)
+                .sorted(comparator)
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -19,8 +19,8 @@ public class ProfilesTest {
         Profiles pr = new Profiles();
         List<Address> rsl = pr.collect(profileList);
         List<Address> expected = new ArrayList<>();
-        expected.add(new Address("London", "Queen street", 10, 4));
         expected.add(new Address("", "Arbat street", 14, 5));
+        expected.add(new Address("London", "Queen street", 10, 4));
         assertThat(rsl, is(expected));
     }
 
@@ -46,6 +46,24 @@ public class ProfilesTest {
         Profiles pr = new Profiles();
         List<Address> rsl = pr.collect(profileList);
         List<Address> expected = new ArrayList<>();
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenCollect5profilesWithDuplicates() {
+        List<Profile> profileList = List.of(
+                new Profile(new Address("Tokyo", "Yakuza street", 1, 3)),
+                new Profile(new Address("London", "Queen street", 10, 4)),
+                new Profile(new Address("Tokyo", "Yakuza street", 1, 3)),
+                new Profile(new Address("Moscow", "Arbat street", 14, 5)),
+                new Profile(new Address("London", "Queen street", 10, 4))
+        );
+        Profiles pr = new Profiles();
+        List<Address> rsl = pr.collect(profileList);
+        List<Address> expected = new ArrayList<>();
+        expected.add(new Address("London", "Queen street", 10, 4));
+        expected.add(new Address("Moscow", "Arbat street", 14, 5));
+        expected.add(new Address("Tokyo", "Yakuza street", 1, 3));
         assertThat(rsl, is(expected));
     }
 }
